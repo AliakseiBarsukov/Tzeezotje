986c1e82f4145d3d35f838833da69216c192f665
Aliaksei Barsukov <barsecky@gmail.com>
2023-08-02 15:50:43 +0300

    add modal form

--- js/style.js
+++ js/style.js
@@ -102,3 +102,59 @@ const menuClose = () => {
 
 menuOpen();
 menuClose();
+
+
+
+
+const modalController = ({modal, btnOpen, btnClose, modalSubmit, time = 300}) => {
+  const buttonElems = document.querySelectorAll(btnOpen);
+  const modalElem = document.querySelector(modal);
+
+  modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity ${time}ms ease-in-out;
+  `;
+
+  const closeModal = event => {
+    const target = event.target;
+
+    if (
+      target === modalElem ||
+      (btnClose && target.closest(btnClose)) ||
+      // (modalSubmit && target.closest(modalSubmit)) ||
+      event.code === 'Escape'
+      ) {
+      
+      modalElem.style.opacity = 0;
+
+      setTimeout(() => {
+        modalElem.style.visibility = 'hidden';
+        scrollController.enabledScroll();
+      }, time);
+
+      window.removeEventListener('keydown', closeModal);
+    }
+  }
+
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown', closeModal);
+    scrollController.disabledScroll();
+  };
+
+  buttonElems.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  modalElem.addEventListener('click', closeModal);
+};
+
+modalController({
+  modal: '.modal', 
+  btnOpen: '.header__btn', 
+  btnClose: '.modal__close', 
+  modalSubmit: '.modal__submit',
+});
